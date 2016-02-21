987aa0a0323a11f62224e9c2262752e25548d33a
Simon Hamermesh <simonhamermesh@Zipcoder12s-MBP.home>
2016-02-21 17:26:09 -0500

    Part One

--- src/main/java/DataValidator.java
+++ src/main/java/DataValidator.java
@@ -4,7 +4,17 @@
 public class DataValidator {
 
     public static boolean isValidUsername(String x){
-        return false;
+
+        if(x.length()>=3 && x.length()<=25){
+          char[] stringToChars = x.toCharArray();
+            for(char y : stringToChars){
+                int charInt = (int) y;
+                if(charInt>=97 && charInt<=122){
+                    //Intended empty body, do nothing, check next char.
+                } else {return false;}
+            }
+            return true;
+        } else {return false;}
     }
 
     public static boolean isValidEnhancedUsername(String x){
